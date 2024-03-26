36c0874572ada5ef2498bc666fa4b7548d4d16ea
creativeblocker <creativeteas2@gmail.com>
2024-03-26 10:51:28 -0400

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const Fastify = require("fastify");
 const server = Fastify();
 
 server.register(require("@fastify/http-proxy"), {
-	upstream: "https://shuttleproxy.com",
+	upstream: "https://browser.rammerhead.org",
 	prefix: "/",
 	http2: false,
 });
